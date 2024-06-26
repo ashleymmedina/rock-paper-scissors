f0927d26a6dc05408603c64cfa74e0222d5a368e
Ashley Meza <ashley.meza-medina@csedge.org>
2024-06-26 14:09:48 -0700

    continuation

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,3 +1,38 @@
 # Play command line Rock-Paper-Scissors
 # By: Ashley Meza
 
+import random
+# rps = ["rock", "paper", "scissors"]
+
+player1_choice = input("Make your choice, Rock, paper or scissors: ")
+player2_choice = input("Make your choice, Rock, paper, or scissors: ") #random.choice(rps)
+
+if player1_choice == "rock":
+    if player2_choice == "scissors":
+        print("Player 1 won!")
+    if player2_choice == "paper":
+        print("Player 2 won")
+    if player2_choice == "rock":
+        print("You tied!")
+
+if player1_choice == "scissors":
+    if player2_choice == "rock":
+        print("Player 2 won!")
+    if player2_choice == "paper":
+        print("Player 1 won!")
+    if player2_choice == "scissors":
+        print("You tied!")
+
+
+if player1_choice == "paper":
+    if player2_choice == "rock":
+        print("Player 2 won!")
+    if player2_choice == "scissors":
+        print("Player 1 won!")
+    if player2_choice == "paper":
+        print("You tied!")
+
+              
+
+
+
